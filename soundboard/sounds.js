f2288897830b15d366e43c4ed2fc605ba1948cb4
T N <theexp111@gmail.com>
2019-04-22 16:31:05 +0200

    meme
    review

---
+++ soundboard/sounds.js
@@ -0,0 +1,11 @@
+var sounds = {
+    Pascal: [
+        "oof",
+        "despacito",
+        "kennt ihr das",
+        "natürlich Tobi",
+        "pinguinbifi",
+        "schlafgeräusche",
+        "sneak attack"
+    ]
+}
